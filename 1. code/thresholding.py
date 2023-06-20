4021546f6097c0670aa91d1adb4bd94940cf2d28
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-06-20 19:48:32 +0300

    add thresholding funcs

---
+++ 1. code/thresholding.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+from skimage.filters import threshold_otsu
+
+def otsu(image):
+    """otsu thresholding
+
+    Parameters
+    ----------
+    image : OpenCV image (grayscale)
+        image to threshold. should be grayscale
+
+    Examples
+    --------
+    binary,thresh=otsu(image)
+    fig, axes = plt.subplots(ncols=3, figsize=(8, 2.5))
+    ax = axes.ravel()
+    ax[0] = plt.subplot(1, 3, 1)
+    ax[1] = plt.subplot(1, 3, 2)
+    ax[2] = plt.subplot(1, 3, 3, sharex=ax[0], sharey=ax[0])
+    
+    ax[0].imshow(image, cmap=plt.cm.gray)
+    ax[0].set_title('Original')
+    ax[0].axis('off')
+    
+    ax[1].hist(image.ravel(), bins=256)
+    ax[1].set_title('Histogram')
+    ax[1].axvline(thresh, color='r')
+    
+    ax[2].imshow(binary, cmap=plt.cm.gray)
+    ax[2].set_title('Thresholded')
+    ax[2].axis('off')
+    
+    plt.show()
+
+    """
+    thresh = threshold_otsu(image)
+    binary = image > thresh
+    return binary, thresh
