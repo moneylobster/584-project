3179e7fa578cee913fc10177b40a55f5ea9230e7
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-06-20 18:32:44 +0300

    rename file to fix import problems

---
+++ 1. code/segmentanything.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import cv2
+import sys
+sys.path.append("..")
+#from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+import segment_anything
+
+sam_checkpoint = "sam_vit_h_4b8939.pth"
+model_type = "vit_h"
+device = "cuda"
+
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+
+def do_all(image):
+    """segment entire image
+
+    Parameters
+    ----------
+    image : OpenCV image
+        image to segment
+
+    Examples
+    --------
+    masks=do_all(image)
+    plt.figure(figsize=(20,20))
+    plt.imshow(image)
+    show_anns(masks)
+    plt.axis('off')
+    plt.show() 
+
+    """
+    
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+    sam.to(device=device)
+    
+    mask_generator = SamAutomaticMaskGenerator(sam)
+    
+    masks = mask_generator.generate(image)
+
+    return masks
+
+def do_bbox(image, bbox_coords):
+    """segment with bbox
+
+    Segment with bounding box coordinates
+
+    Parameters
+    ----------
+    image : OpenCV image
+        image to segment
+    bbox_coords : 4-element numpy array
+        [x1 y1 x2 y2] of bounding box
+
+    Examples
+    --------
+    masks=do_bbox(image,bbox_coords)
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image)
+    show_mask(masks[0], plt.gca())
+    show_box(input_box, plt.gca())
+    plt.axis('off')
+    plt.show()
+
+    """
+    
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+    sam.to(device=device)
+    
+    predictor = SamPredictor(sam)
+
+    predictor.set_image(image)
+
+    masks, _, _ = predictor.predict(
+        point_coords=None,
+        point_labels=None,
+        box=bbox_coords[None, :],
+        multimask_output=False,
+    )
+
+    return masks
