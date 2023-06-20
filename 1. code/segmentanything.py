d1b763dbecae38b5de662d0f421e4f4e903806a5
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-06-20 19:12:33 +0300

    add missing SAM functions

--- 1. code/segmentanything.py
+++ 1. code/segmentanything.py
@@ -1,11 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import torch
-import cv2
-import sys
-sys.path.append("..")
-#from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
-import segment_anything
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
 
 sam_checkpoint = "sam_vit_h_4b8939.pth"
 model_type = "vit_h"
@@ -26,6 +21,26 @@ def show_anns(anns):
         img[m] = color_mask
     ax.imshow(img)
 
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+    
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+    
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
+    
 def do_all(image):
     """segment entire image
 
@@ -72,7 +87,7 @@ def do_bbox(image, bbox_coords):
     plt.figure(figsize=(10, 10))
     plt.imshow(image)
     show_mask(masks[0], plt.gca())
-    show_box(input_box, plt.gca())
+    show_box(bbox_coords, plt.gca())
     plt.axis('off')
     plt.show()
 
