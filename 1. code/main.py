4eb3c38312813ca4c570cd161d1d23615882d7b4
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-06-22 22:52:25 +0300

    added otsu

--- 1. code/main.py
+++ 1. code/main.py
@@ -21,6 +21,9 @@ def main(imfile):
     imfile : filepath
         path to image to segment
     """
+    plot_h=3
+    plot_w=2
+    
     img=cv2.imread(imfile)
 
     # get a bounding box
@@ -34,7 +37,7 @@ def main(imfile):
     plt.figure(figsize=(10,10))
     
     # original image
-    plt.subplot(2,2,1)
+    plt.subplot(plot_h,plot_w,1)
     plt.imshow(img)
     plt.axis('off')
     plt.title("Original Image")
@@ -43,33 +46,35 @@ def main(imfile):
     # segment anything
     print("Running Segment Anything...")
     seg_res=segmentanything.do_bbox(img, np.array(bbox))
-    plt.subplot(2,2,2)
+    plt.subplot(plot_h,plot_w,2)
     plt.imshow(img)
     segmentanything.show_mask(seg_res[0], plt.gca())
     segmentanything.show_box(bbox, plt.gca())
     plt.axis('off')
-    plt.title("sam")
+    plt.title("Segment Anything")
 
     # k-means 5-D
     print("Running k-means (5D)...")
     kmean5_res=kMeans_meanShift.Kmeans5d_segment(img, cluster_num)
-    plt.subplot(2,2,3)
+    plt.subplot(plot_h,plot_w,3)
     plt.imshow(kmean5_res)
     plt.axis('off')
-    plt.title("kmeans 5d")
+    plt.title("k-means (5D)")
 
     # k-means 3-D
     print("Running k-means (3D)...")
     kmean3_res=kMeans_meanShift.Kmeans3d_segment(img, cluster_num)
-    plt.subplot(2,2,4)
+    plt.subplot(plot_h,plot_w,4)
     plt.imshow(kmean3_res)
     plt.axis('off') 
-    plt.title("kmeans 3d")
+    plt.title("k-means (3D)")
 
     # otsu
-    otsu_res, otsu_thresh=thresholding.otsu(img)
-    plt.subplot(2,2,5)
-    plt.imshow(otsu_res)
+    print("Running Otsu...")
+    img_gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    otsu_res, otsu_thresh=thresholding.otsu(img_gray)
+    plt.subplot(plot_h,plot_w,5)
+    plt.imshow(cv2.cvtColor(otsu_res.astype(np.uint8),cv2.COLOR_GRAY2RGB)*255)
     plt.axis('off')
     plt.title("Otsu")
 
