2df22a770fe9b25fc93f28bd0b6ec6af8677e7f9
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-06-22 18:29:53 +0300

    sam implement

--- 1. code/main.py
+++ 1. code/main.py
@@ -4,6 +4,7 @@ get an image file and show segmentation results from multiple methods
 
 import matplotlib.pyplot as plt
 import cv2
+import numpy as np
 
 # models
 import segmentanything
@@ -21,7 +22,18 @@ def main(imfile):
         path to image to segment
     """
     img=cv2.imread(imfile)
+
+    # get a bounding box
+    print("Hold down the mouse to select a box, then press ESC")
+    bbox=bounding_box.start(img)
+    print(f"Bounding box coordinates: {bbox}")
     
     # segment anything
-    bbox=bounding_box.start(img)
-    print(bbox)
+    print("Running Segment Anything...")
+    seg_res=segmentanything.do_bbox(img, np.array(bbox))
+    plt.figure(figsize=(10, 10))
+    plt.imshow(img)
+    segmentanything.show_mask(seg_res[0], plt.gca())
+    segmentanything.show_box(bbox, plt.gca())
+    plt.axis('off')
+    plt.show()
