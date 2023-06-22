151b5385aecacf254787d260a2c4e9673776e867
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-06-22 21:24:47 +0300

    idk added some stuff

--- 1. code/main.py
+++ 1. code/main.py
@@ -24,29 +24,39 @@ def main(imfile):
     img=cv2.imread(imfile)
 
     # get a bounding box
-    # print("Hold down the mouse to select a box, then press ESC")
-    # bbox=bounding_box.start(img.copy())
-    # print(f"Bounding box coordinates: {bbox}")
+    print("Hold down the mouse to select a box, then press ESC")
+    bbox=bounding_box.start(img.copy())
+    print(f"Bounding box coordinates: {bbox}")
 
     # get number of clusters
     cluster_num=int(input("Enter the number of clusters to segment into: "))
     
     # segment anything
-    # print("Running Segment Anything...")
-    # seg_res=segmentanything.do_bbox(img, np.array(bbox))
+    print("Running Segment Anything...")
+    seg_res=segmentanything.do_bbox(img, np.array(bbox))
     # plt.figure(figsize=(10, 10))
-    # plt.imshow(img)
-    # segmentanything.show_mask(seg_res[0], plt.gca())
-    # segmentanything.show_box(bbox, plt.gca())
-    # plt.axis('off')
-    # plt.show()
+    plt.subplots()
+    plt.imshow(img)
+    segmentanything.show_mask(seg_res[0], plt.gca())
+    segmentanything.show_box(bbox, plt.gca())
+    plt.axis('off')
+    plt.show()
 
-    # k-means
-    print("Running k-means...")
+    # k-means 5-D
+    print("Running k-means (5D)...")
     kmean_res=kMeans_meanShift.Kmeans_segment(img, cluster_num)
-    plt.figure(figsize=(10, 10))
+    # plt.figure(figsize=(10, 10))
     plt.imshow(cv2.cvtColor(kmean_res.astype(np.uint8), cv2.COLOR_HSV2RGB))
     plt.axis('off')
     plt.show()
+
+    # k-means 3-D
+    # print("Running k-means (3D)...")
+    # kmean_res=kMeans_meanShift.Kmeans3d_segment(img, cluster_num)
+    # plt.figure(figsize=(10, 10))
+    # plt.imshow(cv2.cvtColor(kmean_res.astype(np.uint8), cv2.COLOR_HSV2RGB))
+    # plt.axis('off')
+    # plt.show()
+
 
 main("images/balls.png")
